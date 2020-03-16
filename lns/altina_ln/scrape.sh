d07ae81de9e87ed20842af5d00a6e109ad7ab834
altunixon <61726544+altunixon@users.noreply.github.com>
2020-03-16 22:55:43 +0900

    sed md lnbrk

--- lns/altina_ln/scrape.sh
+++ lns/altina_ln/scrape.sh
@@ -4,3 +4,7 @@ for i in $(curl http://skythewood.blogspot.com/p/altina-sword-princess.html | tr
   o=$(echo "$i" | awk -F '|' '{print $2}' | sed 's/\:/_/g; s/ /_/g; s/&nbsp;/_/g; s/&amp;/_/g; s/&#8217;/_/g; s/\//_/g; s/__/_/g')
   curl -L "$l" | sed -n '/<h3/,/post-footer/p' > "$o"
  done
+
+
+sed -i '/^$/d; s/\.$/\.<br\/>/g; s/"$/"<br\/>/g; s/】$/"<br\/>/g; s/【/"/g; s/』$/"<br\/>/g; s/『/"/g' *.md
+sed -i 's/^Altina the Sword Princess/### Altina the Sword Princess/g; s/^Translators/#### Translators/g; s/^Editor/#### Editor/g' *.md

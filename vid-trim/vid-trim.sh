a592a951931d0ae6676398579b12f500227baad7
altunixon <61726544+altunixon@users.noreply.github.com>
2020-04-14 11:12:25 +0900

    Create vid-trim.sh
    un tested, logically sound ${@:2} # from position 2 until end

---
+++ vid-trim/vid-trim.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+path_out=${1:-'./'}
+path_temp="/tmp"
+temp_img="${path_temp}/screenshot-trim.jpg"
+trim_fuzz="25%"
+
+for v_in in ${@:2}; do
+    # Get file name and extension
+    v_base=$(basename -- "$v_in")
+    v_name="${v_base%.*}"
+    v_ext="${v_base##*.}"
+    v_out="${path_out%%/}/${v_name}-cropped.${v_ext}"
+    # Copy original video to temp file, reduce disk usage, should probly disable for larger files
+    temp_vid="${path_temp}/vid-in.${v_ext}"
+    cp -rf "$v_in" "$temp_vid"
+    
+    v_resolution=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$temp_vid")
+    # Take screenshot
+    ffmpeg -ss 00:00:30 -i "$temp_vid" -vframes 1 -q:v 5 "$temp_img"
+    # Trim screenshot img
+    mogrify -trim -fuzz $trim_fuzz "$temp_img"
+    # Detect final dimensions
+    $ss_geo=$(magick identify "$temp_img" | awk '{print $3}')
+    $ss_w=$(echo "$ss_geo" | awk -F 'x' '{print $1}')
+    $ss_h=$(echo "$ss_geo" | awk -F 'x' '{print $2}')
+    # Crop video to final dimensions
+    echo -e "Trim: '$v_base' [$v_resolution] => [$ss_geo]"
+    ffmpeg -i "$temp_vid" -filter:v "crop=$ss_w:$ss_h" -c:a copy "$v_out"
+    # Cleanup temp files
+    rm "$temp_img" "$temp_vid"
+done
+
+# For Filename
+# echo "a.b.c.txt" | rev | cut -d"." -f2-  | rev
+# For extension
+# echo "a.b.c.txt" | rev | cut -d"." -f1  | rev

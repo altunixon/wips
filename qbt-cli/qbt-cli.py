2a7f9c7cae3b843a5dafa38b0cd427195f362e22
altunixon <61726544+altunixon@users.noreply.github.com>
2020-04-24 13:07:15 +0900

    Update qbt-cli.py

--- qbt-cli/qbt-cli.py
+++ qbt-cli/qbt-cli.py
@@ -15,27 +15,38 @@ if __name__ == '__main__':
         dest='list_trnt', nargs='*', default=[],
         help='List of Torrents to process, could be a mix of file paths, urls, or magnets')
     parser.add_argument('--api', 
-        dest='url_api', type=str, default='127.0.0.1:8080'
-        help='API\'s URL')
+        dest='url_api', type=str, default='http://127.0.0.1:8080/api/v2/torrents/add'
+        help='API\'s URL, default "http://127.0.0.1:8080/api/v2/torrents/add"')
     parser.add_argument('-o', '--output', 
         dest='path_out', type=str, default=None, 
-        help='Save path')
+        help='Save path, default unspecified, uses application\'s default save location')
+    parser.add_argument('-t', '--tag', 
+        dest='name_tag', type=str, default=None, 
+        help='Tag torrents under "Category", default no tagging')
     parser.add_argument('-s', '--start', dest = 'auto_start', action = 'store_true')
     parser.set_defaults(auto_start = False)
     console_args = parser.parse_args()
     
-    data_post = []
+    data_post = {}
     data_urls = set([])
-    data_magnets = set([])
     for a_trnt in console_args.list_trnt:
         if '://' in a_trnt or 'magnet:' in a_trnt:
             data_urls.add(a_trnt)
         else:
             if os.path.isfile(a_trnt):
+                if 'torrents' not in data_post.keys():
+                    data_post['torrents'] = []
+                else:
+                    pass
                 # ('images', ('foo.png', open('foo.png', 'rb'), 'image/png'))
                 a_file = os.path.basename(a_trnt)
                 with open(a_trnt, 'rb') as byte_data:
-                    data_post.append(('torrents', (a_file, byte_data, 'application/x-bittorrent')))
-    data_post.append(('urls', tuple(list(data_urls))))
-        
+                    data_post['torrents].append(tuple(a_file, byte_data, 'application/x-bittorrent'))
+    data_post['urls'] = tuple(list(data_urls))
+    if console_args.path_out is not None and len(console_args.path_out) > 0:
+        data_post['savepath'] = console_args.path_out
+    else:
+        pass
+
+    data_encoder = MultipartEncoder(fields=data_post)
     

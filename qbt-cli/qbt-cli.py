142f14df0467f8bdcb2561dea3552184fc040da4
alt@shuttle-dh310v2 <altunixon@gmail.com>
2020-05-10 12:31:06 +0900

    NU scraper auto commit [Sun May 10 12:31:03 JST 2020]

--- qbt-cli/qbt-cli.py
+++ qbt-cli/qbt-cli.py
@@ -3,6 +3,10 @@
 import requests, os, json
 from requests_toolbelt import MultipartEncoder
 
+# qb v4.0.3 nox
+# https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-Documentation
+# POST /command/upload HTTP/1.1
+
 # FUNCTIONS
 
 # END FUNCTIONS
@@ -16,8 +20,8 @@ if __name__ == '__main__':
         dest='list_trnt', nargs='*', default=[],
         help='List of Torrents to process, could be a mix of file paths, urls, or magnets')
     parser.add_argument('--api', 
-        dest='url_api', type=str, default='http://127.0.0.1:8080/api/v2/torrents/add', 
-        help='API\'s URL, default "http://127.0.0.1:8080/api/v2/torrents/add"')
+        dest='url_api', type=str, default='http://127.0.0.1:8080/command/upload', 
+        help='API\'s URL, default "http://127.0.0.1:8080/command/upload"')
     parser.add_argument('-o', '--output', 
         dest='path_out', type=str, default=None, 
         help='Save path, default unspecified, uses application\'s default save location')
@@ -30,45 +34,36 @@ if __name__ == '__main__':
     console_args = parser.parse_args()
     
     data_post = {}
-    data_urls = set([])
-    # for a_trnt in console_args.list_trnt:
-    #     if '://' in a_trnt or 'magnet:' in a_trnt:
-    #         data_urls.add(a_trnt)
-        # else:
-        #     if os.path.isfile(a_trnt):
-        #         # if 'torrents' not in data_post.keys():
-        #         #     data_post['torrents'] = []
-        #         # else:
-        #         #     pass
-        #         # ('images', ('foo.png', open('foo.png', 'rb'), 'image/png'))
-        #         a_file = os.path.basename(a_trnt)
-        #         print (a_file)
-        #         with open(a_trnt, 'rb') as byte_data:
-        #             # data_post['torrents'].append((a_file, byte_data, "application/x-bittorrent"))
-        #     else:
-        #         pass
-    # data_post['urls'] = tuple(list(data_urls))
     if console_args.path_out is not None and len(console_args.path_out) > 0:
         data_post['savepath'] = console_args.path_out
     if console_args.name_tag is not None and len(console_args.name_tag) > 0:
         data_post['category'] = console_args.name_tag
     data_post['paused'] = 'true' if not console_args.auto_start else 'false'
-    data_post['torrents'] = (
-        os.path.basename(console_args.list_trnt[0]), 
-        open(console_args.list_trnt[0], 'rb'), 
-        "application/x-bittorrent"
-    )
-    print (data_post.keys())
-    data_encoded = MultipartEncoder(fields=data_post)
-    data_response = requests.post(
-        console_args.url_api,
-        data = data_encoded, 
-        headers = {'Content-Type': data_encoded.content_type}
-    )
-    print ('[#{status}] Content:\n{response}'.format(
-        status = data_response.status_code, 
-        response = json.dumps(data_response.header, indent=4, sort_keys=True, ensure_ascii=False)
-        )
-    )
+
+    data_urls = set([])
+    for a_trnt in console_args.list_trnt:
+        if '://' in a_trnt or 'magnet:' in a_trnt:
+            data_urls.add(a_trnt)
+        else:
+            if os.path.isfile(a_trnt):
+                data_post['torrents'] = (
+                    os.path.basename(a_trnt), 
+                    open(a_trnt, 'rb'), 
+                    "application/x-bittorrent"
+                )
+                data_encoded = MultipartEncoder(fields=data_post)
+                data_response = requests.post(
+                    console_args.url_api,
+                    data = data_encoded, 
+                    headers = {'Content-Type': data_encoded.content_type}
+                )
+                print ('[#{status}] Content:\n{response}'.format(
+                    status = data_response.status_code, 
+                    response = data_response.headers
+                    # response = json.dumps(data_response.headers, indent=4, sort_keys=True, ensure_ascii=False)
+                    )
+                )
+            else:
+                print ('[ERR_] File "%s" missing' % a_trnt)
 else:
     pass

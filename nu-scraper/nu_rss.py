9a10d8d8641cff674785275c8c11af632defade4
altunixon <61726544+altunixon@users.noreply.github.com>
2020-05-15 13:19:24 +0900

    Update nu_rss.py

--- nu-scraper/nu_rss.py
+++ nu-scraper/nu_rss.py
@@ -8,6 +8,7 @@ from helpers.misc import countdown
 from helpers.dir_helper import MkDirP
 from browsers.selenium import SeleniumBrowser
 from browsers.scraper_lxml import html_scraper
+from html2md import html2md
 
 nu_prefix = 'https://www.novelupdates.com/series/'
 xpath_nu_navigation = '//a[@class="next_page"]'
@@ -32,30 +33,6 @@ def chapter_zfill(chapter_txt, **zargs):
         return chapter_fill
     else:
         return None
-    
-def html2md(text_html, **kargs):
-    link_ref = kargs.get('refer', None)
-    link_desc = kargs.get('desc', 'Link')
-    soup_features = kargs.pop('features', 'lxml')
-    soup_html = BeautifulSoup(text_html, features=soup_features)
-    # kill all script and style elements
-    for x in soup_html.find_all("div", {'class': 'sharedaddy'}):
-        x.decompose() # broke merc
-    for soup_script in soup_html(["script", "style", "ins"]):
-        soup_script.decompose()    # rip it out
-    # soup_text = soup_html.get_text()
-    # soup_text = re.sub('(\t|\s+)?\n+', '\n', soup_html.get_text(), re.MULTILINE) # WTF no break!?
-    soup_text = re.sub('\n\s*\n', '\n', soup_html.get_text())
-    # decompose broke mercenary wordads c131
-    soup_text = soup_text.replace('\n', '<br/>\n')
-    if link_ref is not None:
-        return '[{desc}]({refer})\n<br/>{md}'.format(
-            desc = link_desc, 
-            refer = link_ref, 
-            md = soup_text
-        )
-    else:
-        return soup_text
 
 def chapter_md(chapter_link, **kargs):
     xpath_article = kargs.get('article', None)

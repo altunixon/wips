692a9e64f9bd0d70d8db566c563c08ac7dc7d86a
altunixon <61726544+altunixon@users.noreply.github.com>
2020-07-27 16:58:27 +0900

    Update curl-se.py

--- scripts/nu-scraper/curl-se.py
+++ scripts/nu-scraper/curl-se.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-import requests, os, re, json, feedparser, time
-from lxml import html
+import os
 from bs4 import BeautifulSoup
-from helpers.str_helper import urlPrefixer, string_sanitizer
-from helpers.misc import countdown
 from browsers.selenium import SeleniumBrowser
 from browsers.scraper_lxml import html_scraper
-from html2md import html2md
 
 if __name__ == '__main__':
     try:
@@ -24,7 +20,9 @@ if __name__ == '__main__':
         parser.add_argument('-o', '--output', 
             dest = 'path_out', type = str, 
             default = None, 
-            help = 'Output dir')
+            help = 'Output path')
+        parser.add_argument('-b', '--bs4', dest = 'soup_out', action = 'store_true')
+        parser.set_defaults(soup_out = False)
         parser.add_argument('-x', '--xpath', 
             dest = 'xpath', type = str, 
             default = None, 
@@ -32,7 +30,7 @@ if __name__ == '__main__':
         parser.add_argument('-w', '--wait', 
             dest = 'time_wait', type = int, 
             default = 3, 
-            help = 'Wait time in-between curls')
+            help = 'Wait for page to load before reading source HTML.')
         console_args = parser.parse_args()
         if len(console_args.url) > 0:
             url_haz_javascript = console_args.url[0]
@@ -43,12 +41,19 @@ if __name__ == '__main__':
                     url_html, refer = url_haz_javascript, tag_content = console_args.xpath
                 )['tag_content']
                 if len(url_article) > 0:
-                    url_content = url_article[0].decode(encoding='UTF-8') # type bytes!? WTF, temporary using decode to convert to str
+                    url_content = url_article[0].decode(encoding='UTF-8')
+                    # type bytes!? lxml! WTF!?, temporary using decode to convert to str
+                    # fixed in browsers.scraper_lxml, maybe...
                 else:
-                    url_content = url_haz_javascript
+                    url_content = 'URL : "%s"\nXpath: <%s>\nNOTFOUND\n' % (url_haz_javascript, console_args.xpath)
             else:
                 url_content = url_html
-            print (type(url_content))
+            # print (type(url_content))
+            if console_args.soup_out:
+                soup_content = BeautifulSoup(url_content, 'html.parser')
+                url_content = soup_content.prettyfy()
+            else:
+                pass
             if console_args.path_out is None:
                 print (url_content)
             else:

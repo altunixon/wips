425331f1cc25ee5a433f18d196e37383c9bcb4c3
altunixon <61726544+altunixon@users.noreply.github.com>
2020-08-14 12:57:43 +0900

    Update patchme.sh

--- scripts/patchme.sh
+++ scripts/patchme.sh
@@ -10,11 +10,14 @@ patch_name="$(echo -n "$file_2b_patch" | awk -F '/' '{print $NF}')"
 
 if [ -f $file_2b_patch ] && [ -f $file_be_latest ]; then
     patch_file="/tmp/${patch_name}.patch"
+    [ -f "$patch_file" ] && rm "$patch_file"
     diff -u $file_2b_patch $file_be_latest | tee "$patch_file"
     echo "Copy: $patch_file => ${patch_name}.patch"
-    cp --no-clobber -v "$patch_file" ./${patch_name}.patch && \
-        patch -u -b $file_2b_patch -i $patch_file || \
-        echo "Patch file '${patch_name}.patch' already exists in current directory, refuse to patch."
+    cp -f -v "$patch_file" ./${patch_name}.patch
+    [ $? -eq 0 ] && patch -u -b $file_2b_patch -i $patch_file || \
+        echo "Patch file '${patch_name}.patch' Creation failed."
+elif [ $# -le 1 ]; then
+    echo -e "$help_msg"
 else
     echo -e "$help_msg\nCheck if both file exists:\n\t2b_patch: '$file_2b_patch'\n\tlatest  : '$file_be_latest'\n"
 fi

71f09866ef2781fdc28f54562b86122cde1949c0
altunixon <61726544+altunixon@users.noreply.github.com>
2021-02-15 11:31:24 +0900

    Update jsondb.py

--- scripts/py-jsondb/jsondb.py
+++ scripts/py-jsondb/jsondb.py
@@ -7,10 +7,6 @@ from time import time, localtime, strftime
 # only support dictionary data type, which means tables are limit to 2 column
 # flush on table close
 
-class json_table:
-    def __init__(self, table_dict, **options):
-        self.table_name = "PLACEHOLDER"
-
 class json_db:
     def __init__(self, db_root, **options):
         self.db_root = db_root
@@ -25,6 +21,7 @@ class json_db:
                 raise Exception('[JSON DB] Database ROOT_PATH jsondb(db="%s") NotFound.')
         else:
             raise Exception('[JSON DB] Database ROOT_PATH jsondb(db="x") has not been defined.')
+        self.db_dirty_tables = set([])
         self.db_delete_on_drop = options.get('delete_on_drop', True)
         self.db_template = {
             "name": "NOT_NUL",
@@ -32,34 +29,49 @@ class json_db:
             "data": {},
             "create_date": "NUL",
             "modified_date": "NUL",
-            "rows": 0,
-            "dirty": 0
+            "rows": 0
         }
         
     def timestamp(self):
         table_ctime = time()
         return strftime('{epoch} | %Y-%m-%d %H:%M:%S %z%Z'.format(epoch=table_ctime), localtime(table_ctime))
     
-    def table_read(self, table_name, **options)):
-        set_dirty = options.get('dirty', None)
+    def table_read(self, table_name, **options):
+        set_dirty = options.get('dirty', False)
         if table_name not in self.db_tables.keys():
             return self.table_create(table_name)
         else:
             with open(self.db_tables[table_name], 'rt') as fp:
                 table_dict = json.load(fp)
-            if set_dirty is not None:
-                table_dict['dirty'] = int(set_dirty)
+            if set_dirty:
+                self.db_dirty_tables.add(table_name)
             return table_dict
         
     def table_insert(self, table_dict, data_key, data_value, **options):
         mode_update = options.get('update', True)
-        if mode_update or data_key not in table_dict['data'].keys():
+        data_old = None
+        if data_key in table_dict['data'].keys():
+            data_old = table_dict['data'][data_key]
+        if mode_update or data_old is None:
             table_dict['data'][data_key] = data_value
-            table_dict['dirty'] = 1
+            self.db_dirty_tables.add(table_dict['name'])
+            print ('[JSON DB] INSERT or UPDATE into {table}|{key}: "{value_old}" -> "{value_new}" flag Update={uflag}'.format(
+                table=table_dict['name'], key=data_key, 
+                value_old=data_old, value_new=data_value, 
+                uflag=mode_update
+            ))
         else:
-            pass
+            print ('[JSON DB] IGNORE UPDATE into {table}|{key}: "{value_old}" -> "{value_new}" since Update={uflag}'.format(
+                table=table_dict['name'], key=data_key, 
+                value_old=data_old, value_new=data_value, 
+                uflag=mode_update
+            ))
         
-    def table_search(self):
+    def table_search(self, table_dict, data_match, **options):
+        match_exact = options.get('exact', False)
+        match_count = options.get('count', False)
+        if match_exact:
+            pass
         return "PLACEHOLDER"
 
     def table_create(self, table_name, **options):
@@ -72,8 +84,7 @@ class json_db:
                 "data": {},
                 "create_date": table_cdate,
                 "modified_date": table_cdate,
-                "rows": 0,
-                "dirty": 1
+                "rows": 0
             }
             table_file = '{root}/{table}.json'.format(root=self.db_root, table=table_name)
             self.db_tables[table_name] = table_file
@@ -82,8 +93,10 @@ class json_db:
             return self.table_read(table_name)
     
     def table_close(self, table_dict, **options):
-        if table_dict["dirty"] > 0:
+        if table_dict['name'] in self.db_dirty_tables:
             table_path = self.db_tables[table_dict["name"]]
+            table_dict['rows'] = table_dict['data'].keys().count()
+            table_dict['modified_date'] = self.timestamp()
             with open(table_path, "w+") as fp:
                 json.dump(table_dict, fp)
             print ('[JSON DB] Table "%s" Flushed to "%s"' % (table_dict["name"], table_path))

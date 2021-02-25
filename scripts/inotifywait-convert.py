d5a13030f1a78730bc8f4d92b21961ffd09edcce
altunixon <61726544+altunixon@users.noreply.github.com>
2021-02-25 17:23:02 +0900

    Update inotifywait-convert.py

--- scripts/inotifywait-convert.py
+++ scripts/inotifywait-convert.py
@@ -10,6 +10,19 @@ def lst2chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
         
+def match_percentage(string_1, string_2): # JANKY AF
+    set_1 = set([x for x in string_1])
+    set_2 = set([y for y in string_1])
+    if len(string_2) > len(string_1):
+        needles, haystack = set_1, set_2
+    else:
+        needles, haystack = set_2, set_1
+    m = 0
+    for n in needles:
+        if n in haystack:
+            m += 1
+    return (m * 100) / len(haystack)
+        
 if __name__ == "__main__":
     args_parser = argparse.ArgumentParser(
         description='Download URL',
@@ -40,23 +53,25 @@ if __name__ == "__main__":
                 elif watched_event == 'MOVED_TO' and action_after is None:
                     path_after = watched_path
                 else:
-                    print ('[SKIP] Conflict Action: [%s] MOVED_FROM[%s] MOVED_TO[%s]' % (wline, path_before, path_after))
+                    print ('[SKIP_] Conflict Action: [%s] MOVED_FROM[%s] MOVED_TO[%s]' % (wline, path_before, path_after))
                     path_before = path_after = None
                 
                 if path_before is not None and path_after is not None:
+                    name_before = path.dirname(path_before)
                     name_after = path.dirname(path_after)
+                    name_match = match_percentage(name_after, name_before)
                     action_bash = '[ test -d "{before}" ] && mv --no-clobber -v "{before}" "{after}"'.format(before=path_before, after=path_after)
-                    if re.search(name_after, path_before, re.IGNORECASE):
-                        print ('[VALID]: ' +action_bash)
+                    if name_match >= 80:
+                        print ('[VALID] Matched %s%%: MOVED_FROM[%s] MOVED_TO[%s]' % (name_match, name_before, name_after))
                         action_lines.append(action_bash)
                     else:
-                        print ('[SKIP] Conflict? Object Name: MOVED_FROM[%s] MOVED_TO[%s]' % (path_before, path_after))
+                        print ('[SKIP_] Conflict? Object Name: MOVED_FROM[%s] MOVED_TO[%s]' % (path_before, path_after))
                         action_lines.append('# %s' % action_bash)
                     path_before = path_after = None
                 else:
                     pass
             else:
-                print ('[SKIP] NOTDIR Event: %s' % wline)
+                print ('[SKIP_] NOTDIR Event: %s' % wline)
     else:
         pass
     if console_args.act_file is not None and not path.isfile(console_args.act_file):

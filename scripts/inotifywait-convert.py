097ac12dd3551f0f7db129303cbe6c73bf0b2b98
alt@shuttle-dh310v2 <altunixon@gmail.com>
2021-03-10 15:33:02 +0900

    NU scraper auto commit [Wed Mar 10 15:33:02 JST 2021]

--- scripts/inotifywait-convert.py
+++ scripts/inotifywait-convert.py
@@ -9,9 +9,10 @@ def lst2chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-        
+
 def chunker_inpair(watched_lines):
     for x in range(0, len(watched_lines), 2):
+        print (watched_lines[x:x+2])
         watch_before, watch_after = watched_lines[x:x+2]
         event_before, type_before, path_before = watch_before.split(':', 2)
         event_after, type_after, path_after = watch_after.split(':', 2)
@@ -25,20 +26,23 @@ def chunker_inpair(watched_lines):
         else:
             print ('[WARN_] Conflict? matched %s%%: MOVED_FROM[%s] MOVED_TO[%s]' % (name_match, name_before, name_after))
             yield ('# %s' % action_bash)
-        
+
 def chunker_inline(watched_lines):
     action_lines = []
-    path_before = path_after = None
+    path_before = path_after = action_previous  = None
     for wline in watched_lines:
+        print (wline)
         watched_event, watched_type, watched_path = wline.split(':', 2)
-        if watched_event == 'MOVED_FROM' and action_before is None:
+        if watched_event == 'MOVED_FROM' and action_previous is None:
             path_before = watched_path
-        elif watched_event == 'MOVED_TO' and action_after is None:
+            action_previous = watched_event
+        elif watched_event == 'MOVED_TO' and action_previous == 'MOVED_FROM':
             path_after = watched_path
+            action_previous = None
         else:
-            print ('[SKIP_] Conflict Action: [%s] MOVED_FROM[%s] MOVED_TO[%s]' % (wline, path_before, path_after))
-            path_before = path_after = None
-        
+            print ('[SKIP_] Conflict Action: [%s/%s] MOVED_FROM[%s] MOVED_TO[%s]' % (action_previous, watched_event, path_before, path_after))
+            path_before = path_after = action_previous = None
+
         if path_before is not None and path_after is not None:
             name_before = path.dirname(path_before)
             name_after = path.dirname(path_after)
@@ -50,11 +54,11 @@ def chunker_inline(watched_lines):
             else:
                 print ('[WARN_] Conflict? Object Name: MOVED_FROM[%s] MOVED_TO[%s]' % (path_before, path_after))
                 action_lines.append('# %s' % action_bash)
-            path_before = path_after = None
+            path_before = path_after = action_previous = None
         else:
             pass
     return action_lines
-        
+
 def match_percentage(string_1, string_2): # JANKY AF
     set_1 = set([x for x in string_1])
     set_2 = set([y for y in string_1])
@@ -66,22 +70,23 @@ def match_percentage(string_1, string_2): # JANKY AF
     for n in needles:
         if n in haystack:
             m += 1
+    print (m)
     return (m * 100) / len(haystack)
-        
+
 if __name__ == "__main__":
     args_parser = argparse.ArgumentParser(
         description='Download URL',
-        argument_default=argparse.SUPPRESS, 
+        argument_default=argparse.SUPPRESS,
         formatter_class=RawTextHelpFormatter)
     args_parser.add_argument(
-        '-i', '--in-file', dest='watch_file', nargs='?', default=None, 
+        '-i', '--in-file', dest='watch_file', nargs='?', default=None,
         help='inotifywait file')
     args_parser.add_argument(
-        '-o', '--out-file', dest='act_file', nargs='?', 
-        default=path.join(getcwd(), 'downloads'), 
+        '-o', '--out-file', dest='act_file', nargs='?',
+        default=path.join(getcwd(), 'downloads'),
         help='Output file')
     args_parser.add_argument(
-        '--all', dest='event_all', action='store_true', 
+        '--all', dest='event_all', action='store_true',
         help='Match ALL events, not just ISDIR (Currently ONLY support ISDIR)')
     args_parser.set_defaults(debug=False)
     # Parse Console Args
@@ -90,10 +95,10 @@ if __name__ == "__main__":
         print (console_args.watch_file)
         with open(console_args.watch_file, 'r') as wf:
             wls = wf.readlines()
-        print (len(wls))
+        # print (len(wls))
         watched_lines = [line for line in wls if line.find('ISDIR') >= 0]
-        # action_lines = chunker_inline(watched_lines)
-        action_lines = chunker_inpair(watched_lines)
+        action_lines = chunker_inline(watched_lines)
+        # action_lines = chunker_inpair(watched_lines)
     else:
         print ('XXX')
         pass

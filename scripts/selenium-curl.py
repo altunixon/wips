a3c1b2a30c2288ce42586f09bafc54eaffbafbea
altunixon <61726544+altunixon@users.noreply.github.com>
2021-05-11 16:27:35 +0900

    Update selenium-curl.py

--- scripts/selenium-curl.py
+++ scripts/selenium-curl.py
@@ -6,8 +6,19 @@ import os, time, urllib, json, re
 from lxml                   import html
 from browsers.selenium      import SeleniumBrowser
 from browsers.login         import page_login_sel
+from browsers.scrapers      import lxmlhtml_scraper
 
 default_path = os.path.dirname(os.path.abspath(__file__))
+
+form_example = {
+    'url': '',
+    'formname': '',
+    'username': '',
+    'formpass': '',
+    'password': '',
+    'submit': '',
+    'home': '',
+}
 
 def login(driver, **form_data):
     loginurl = form_data.get('url', None)
@@ -31,6 +42,9 @@ def login(driver, **form_data):
 #https://twitter.com/sherryken777/media
 def main():
     global console_args, console_color
+    if console_args.env_display is not None:
+        os.environ['DISPLAY'] = ':%s' % console_args.env_display.strip(':')
+
     mybr = SeleniumBrowser(
         private     = console_args.incognito, 
         capability  = console_args.browser, 
@@ -45,20 +59,56 @@ def main():
     )
 
     if console_args.login_json is not None:
-        login(mybr.driver, **json.loads(console_args.login_json))
+        json_type, json_src = console_args.login_json.split(':', 1)
+        if json_type == 'file':
+            assert os.path.isfile(json_src), ('Login form data not found: "%s"' % json_src)
+            with open(json_src, 'r') as jfd:
+                login(mybr.driver, **json.load(jfd))
+        else:
+            login(mybr.driver, **json.loads(json_src))
     else:
         pass
 
     if console_args.py_readable:
         from readability import Document
+        
+    re_filter = None if console_args.result_filter is None else re.compile(console_args.result_filter)
 
     try:
         for url_js in console_args.urls:
             mybr.get(url_js)
             time.sleep(console_args.wait)
             if console_args.py_readable:
-                html_doc = Document(mybr.read())
-            print (html_doc.summary())
+                html_doc = Document(mybr.read()).summary()
+            elif console_args.moz_readable:
+                html_doc = mybr.read()
+            else:
+                html_doc = mybr.read()
+
+            if console_args.links_only or console_args.elem_xpath is not None:
+                if console_args.links_only:
+                    xpath_collection = {'href': '//a', 'src': '//img'}
+                else:
+                    xpath_collection = {}
+                    for x in console_args.elem_xpath.split('|'):
+                        y, z = x.split(':', 1)
+                        xpath_collection[y] = z
+                element_collection = lxmlhtml_scraper(html_doc, refer=url_js, **xpath_collection)
+                scrape_result = []
+                for tag_data, data_value in element_collection.items():
+                    scrape_result.extend([x.strip() for x in data_value if len(x.strip()) > 0])
+
+                if re_filter is not None:
+                    for scrape_d in scrape_result:
+                        if re_filter.search(scrape_d):
+                            print (scrape_d)
+                        else:
+                            pass
+                 else:
+                    print (('\n' if console_args.linebreak else ' ').join(scrape_result))
+            else:
+                print (html_doc)
+            
     except Exception as excp:
         raise excp
 
@@ -89,6 +139,8 @@ if __name__ == '__main__':
     parser.add_argument('--headless', dest='headless', action='store_true',
         help='Headless mode.')
     parser.set_defaults(headless=False)
+    parser.add_argument('--xpath', dest='env_display', nargs='?', default=None,
+        help='Set output display, see /tmp/.X11-unix/X* for available display values')
     parser.add_argument('--read-moz', dest='moz_readable', action='store_true',
         help='Parse html content with readability module, needs nodejs and npm install @mozilla/readability')
     parser.set_defaults(moz_readable=False)
@@ -100,8 +152,11 @@ if __name__ == '__main__':
     parser.set_defaults(links_only=False)
     parser.add_argument('--xpath', dest='elem_xpath', nargs='?', default=None,
         help='Scrape html with xpath, ex: href://a[@id="xxx"]')
+    parser.add_argument('--no-eol', dest='linebreak', action='store_false', 
+        help='Do not print linebreak, only applies to links and xpath flag')
+    parser.set_defaults(linebreak=True)
     parser.add_argument('--regex', dest='result_filter', nargs='?', default=None,
-        help='Post process filter result with regex match, usually uses with links')
+        help='Post process filter result with regex match, usually uses with links, use with other flags may produce unpredictable result')
     parser.add_argument('--login', dest='login_json', nargs='?', default=None,
         help='Login using json, also accept json file, use json:{string} or file:/path/to/file.json')
     parser.add_argument('-q', '--quiet', dest='verbose', action='store_false', 
